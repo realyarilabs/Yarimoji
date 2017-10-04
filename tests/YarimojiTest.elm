4e378a5aebb4721e1a1461414473d6365ed1468b
Xavier Francisco <xavier.n.francisco@gmail.com>
2017-10-04 15:46:45 +0100

    Added some unit testing

---
+++ tests/YarimojiTest.elm
@@ -0,0 +1,130 @@
+module YarimojiTest exposing (..)
+
+import Char
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+import Tuple
+import Yarimoji
+    exposing
+        ( emojidb
+        , yariCheckEmoji
+        , yariFindEmoji
+        , yariMojiTranslate
+        )
+
+
+-- , initYmojiModel
+-- , update
+-- , yariMojiTranslate
+-- , yariReplacebyEmoji
+-- , ymojiPickup
+
+
+emojiDBTest : Test
+emojiDBTest =
+    describe "EmojiDBTest"
+        [ test "EmojiDB maps from chars that are ascii" <|
+            \() ->
+                List.map Tuple.second emojidb
+                    |> String.concat
+                    |> String.toList
+                    |> List.map Char.toCode
+                    |> List.all (\c -> c < 128)
+                    |> Expect.true "All emoji come from ascii"
+        , test "EmojiDB mas to chars that are not! ascii" <|
+            \() ->
+                List.map Tuple.first emojidb
+                    |> String.concat
+                    |> String.toList
+                    |> List.map Char.toCode
+                    |> List.all (\c -> c > 128)
+                    |> Expect.true "No emoji has ascii"
+        ]
+
+
+containsEmojiTest : Test
+containsEmojiTest =
+    describe "ContainsEmojiTest"
+        [ test "emoji in string" <|
+            \() ->
+                "Hello guys :D"
+                    |> yariCheckEmoji
+                    |> Expect.true "expect emoji to be found"
+        , test "many emoji in string" <|
+            \() ->
+                "This string :) is the best string :p . >:-o"
+                    |> yariCheckEmoji
+                    |> Expect.true "expect emoji to be found"
+        , test "emoji followed by exclamation point" <|
+            \() ->
+                "Things:p!"
+                    |> yariCheckEmoji
+                    |> Expect.false "expect emoji not to be found"
+        , test "url, has unwanted emoji after protocol" <|
+            \() ->
+                "http://www.google.com"
+                    |> yariCheckEmoji
+                    |> Expect.false "expect emoji not to be found"
+        ]
+
+
+findEmojiTest : Test
+findEmojiTest =
+    describe "FindEmojiTest"
+        [ test "emoji in string" <|
+            \() ->
+                "Hello guys :D"
+                    |> yariFindEmoji
+                    |> List.head
+                    |> Maybe.map Tuple.second
+                    |> Expect.equal (Just ":D")
+        , test "many emoji in string" <|
+            \() ->
+                "This string :) is the best string :p . >:-o"
+                    |> yariFindEmoji
+                    |> List.map Tuple.second
+                    |> Expect.equalLists [ ":)", ":p", ">:-o" ]
+        , test "emoji followed by exclamation point" <|
+            \() ->
+                "Things:p!"
+                    |> yariFindEmoji
+                    |> List.map Tuple.second
+                    |> Expect.equalLists []
+        , test "url, has unwanted emoji after protocol" <|
+            \() ->
+                "http://www.google.com"
+                    |> yariFindEmoji
+                    |> List.map Tuple.second
+                    |> Expect.equalLists []
+        ]
+
+
+translateEmojiTest : Test
+translateEmojiTest =
+    describe "TranslateEmojiTest"
+        [ test "emoji in string" <|
+            \() ->
+                "Hello guys :D"
+                    |> yariFindEmoji
+                    |> yariMojiTranslate "Hello guys :D"
+                    |> Expect.equal "Hello guys 😃"
+        , test "many emoji in string" <|
+            \() ->
+                "This string :) is the best string :p . >:-o"
+                    |> yariFindEmoji
+                    |> yariMojiTranslate "This string :) is the best string :p . >:-o"
+                    |> Expect.equal "This string 😊 is the best string 😛 . 😠"
+        , test "emoji followed by exclamation point" <|
+            \() ->
+                "Things:p!"
+                    |> yariFindEmoji
+                    |> yariMojiTranslate "Things:p!"
+                    |> Expect.equal "Things:p!"
+        , test "url, has unwanted emoji after protocol" <|
+            \() ->
+                "http://www.google.com"
+                    |> yariFindEmoji
+                    |> yariMojiTranslate "http://www.google.com"
+                    |> Expect.equal "http://www.google.com"
+        ]
